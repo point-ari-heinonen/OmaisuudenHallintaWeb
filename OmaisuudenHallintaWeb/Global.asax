6800e6a06ab47e6285a65af4c613432a0c08a134
ari.heinonen@stud.pointcollege.fi <ari.heinonen@stud.pointcollege.fi>
2016-11-09 21:32:09 +0200

    OmaisuudenHallintaWeb eka commit

---
+++ OmaisuudenHallintaWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OmaisuudenHallintaWeb.MvcApplication" Language="C#" %>
---
+++ OmaisuudenHallintaWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OmaisuudenHallintaWeb.Startup))]
+namespace OmaisuudenHallintaWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
